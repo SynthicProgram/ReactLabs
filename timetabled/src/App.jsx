50796405dd4b4a794e7fa7e2fbd2507abada0cf9
Eric <146690262+SynthicProgram@users.noreply.github.com>
2025-02-22 11:56:15 -0800

    Lab1

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Schedule for idk</h1>
+      <h2>Welcome! Idk what this schedule is for yet</h2>
+      <Calendar/>
+    </div>
+  )
+}
+
+export default App
